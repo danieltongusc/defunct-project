b409a6c810edd5c31d6fd80718e77f67c551330a
Daniel Tong <143573000+danieltongusc@users.noreply.github.com>
2024-06-03 19:34:39 -0700

    Adjusted bounce and friction

--- pair_prototype_daniel&dawei_version1.0/Assets/Bullets Left Script.cs
+++ pair_prototype_daniel&dawei_version1.0/Assets/Bullets Left Script.cs
@@ -18,34 +18,12 @@ public class BulletManager : MonoBehaviour
         UpdateBulletText(); // Update UI text on start
     }
 
-    public void FireBullet()
-    {
-        if (currentBullets > 0)
-        {
-            currentBullets--; // Decrease bullet count
-            UpdateBulletText(); // Update the UI text
-            // Add your firing logic here
-        }
-        else
-        {
-            Debug.Log("Out of bullets!");
-        }
-    }
-
-    void UpdateBulletText()
-    {
-        bulletText.text = "Bullets: " + currentBullets; // Update the text display
-    }
-
-    // Method to reload bullets
-    public void ReloadBullets()
-    {
-        currentBullets = maxBullets;
-        UpdateBulletText();
-    }
-
     void Update() {
         currentBullets = gunScript.bulletsLeft;
         UpdateBulletText();
+    }
+    void UpdateBulletText()
+    {
+        bulletText.text = "Bullets: " + currentBullets; // Update the text display
     }
 }
